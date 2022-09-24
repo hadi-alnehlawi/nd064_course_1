ae8e0dcd629f8775eba68386f6f716b645c06c03
Hadi <hadi.alnehlawi@gmail.com>
2022-09-23 22:15:32 -0300

    metric endpoints

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,28 +1,39 @@
+from glob import glob
+import re
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+from flask import jsonify
+
+post_count = 0
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global post_count
+    post_count = post_count + 1
     return connection
+
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id, )).fetchone()
     connection.close()
     return post
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,22 +41,25 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -56,8 +70,9 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute(
+                'INSERT INTO posts (title, content) VALUES (?, ?)',
+                (title, content))
             connection.commit()
             connection.close()
 
@@ -65,6 +80,26 @@ def create():
 
     return render_template('create.html')
 
+
+# Define healthz
+@app.route('/healthz', methods=['GET'])
+def healthz():
+    data = {'result': 'OK - healthy'}
+    return jsonify(data), 200
+
+
+# Define metrics
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    global post_count
+    db_connection_count = 10
+    data = {
+        "db_connection_count": db_connection_count,
+        "post_count": post_count
+    }
+    return jsonify(data), 200
+
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    app.run(host='0.0.0.0', port='3111')
