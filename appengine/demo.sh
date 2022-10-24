d4a2aa4dc364decd929eaaca26dc38bf13048670
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-10-24 18:37:12 +0000

    count

--- appengine/demo.sh
+++ appengine/demo.sh
@@ -1,8 +1,15 @@
 
-export VERSIONS_COUNT=$(gcloud app versions list --sort-by=createTime --format=json | jq ' . | length');
-echo $VERSIONS_COUNT;
-if [[ $VERSIONS_COUNT == 210 ]] 
-then 
-	echo "deleting the last verion which its count is 210...." 
-	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)') --quiet
-fi
+COUNT=0
+for ID in $(gcloud app versions list --sort-by=createTime --format="value(VERSION.ID)")
+do
+   ((COUNT=COUNT+1))
+done
+
+echo $COUNT
+# export VERSIONS_COUNT=$(gcloud app versions list --sort-by=createTime --format=json | jq ' . | length');
+# echo $VERSIONS_COUNT;
+# if [[ $VERSIONS_COUNT == 210 ]] 
+# then 
+# 	echo "deleting the last verion which its count is 210...." 
+# 	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)') --quiet
+# fi
