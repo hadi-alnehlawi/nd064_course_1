c4052fb4dfb68ceb6d3e0622920e2fd97304b0bf
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-10-24 18:52:52 +0000

    updatE

--- appengine/demo.sh
+++ appengine/demo.sh
@@ -10,6 +10,6 @@ echo $VERSIONS_COUNT
 
 if [[ $VERSIONS_COUNT == 4 ]] 
 then 
-	echo "deleting the last verion which its count is 4...." 
+	echo "Deleting the last verion which its count is 4....!!" 
 	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)') --quiet
 fi
