4e2ca537c82049ab6c86003119e252dda9d53fd8
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-10-24 15:09:29 +0000

    add cloudbuild

---
+++ appengine/demo.sh
@@ -0,0 +1,7 @@
+export VERSIONS_COUNT=$(gcloud app versions list --sort-by=createTime --format=json | jq ' . | length');
+echo $VERSIONS_COUNT;
+if [[ $VERSIONS_COUNT == 210 ]] 
+then 
+	echo "deleting the last verion which its count is 210...." 
+	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)') --quiet
+fi
