b8b5159684198d04157ed9741b36ccb78c25ed95
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-10-24 18:47:50 +0000

    update

--- appengine/demo.sh
+++ appengine/demo.sh
@@ -1,15 +1,15 @@
 
-COUNT=0
+VERSIONS_COUNT=0
 for ID in $(gcloud app versions list --sort-by=createTime --format="value(VERSION.ID)")
 do
-   ((COUNT=COUNT+1))
+   ((VERSIONS_COUNT=VERSIONS_COUNT+1))
 done
 
-echo $COUNT
-# export VERSIONS_COUNT=$(gcloud app versions list --sort-by=createTime --format=json | jq ' . | length');
-# echo $VERSIONS_COUNT;
-# if [[ $VERSIONS_COUNT == 210 ]] 
-# then 
-# 	echo "deleting the last verion which its count is 210...." 
-# 	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)') --quiet
-# fi
+echo $VERSIONS_COUNT
+
+
+if [[ $VERSIONS_COUNT == 4 ]] 
+then 
+	echo "deleting the last verion which its count is 4...." 
+	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)') --quiet
+fi
