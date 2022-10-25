4ddad8ed1ba5085109e6110d07771ee86dc765f0
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-10-25 18:46:06 +0000

    update

---
+++ appengine/scripts/cleanup_last_appengine_build.sh
@@ -0,0 +1,15 @@
+VERSIONS_COUNT=0
+SERVICE_NAME=APPENGINE_SERVICE_NAME
+for ID in $(gcloud app versions list --sort-by=createTime --format="value(VERSION.ID) --service=$SERVICE_NAME")
+do
+   ((VERSIONS_COUNT=VERSIONS_COUNT+1))
+done
+
+echo $VERSIONS_COUNT
+
+
+if [[ $VERSIONS_COUNT == 200 ]] 
+then 
+	echo "Deleting the last verion which its count is 200....!!" 
+	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)' --service=$SERVICE_NAME) --quiet
+fi
