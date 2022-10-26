acadf923531fc3149964b45841840a98feb09299
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-10-26 08:53:02 +0000

    s

--- appengine/scripts/cleanup_last_appengine_build.sh
+++ appengine/scripts/cleanup_last_appengine_build.sh
@@ -2,18 +2,21 @@ VERSIONS_COUNT=0
 SERVICE_NAME=default
 VERSION_MAX=2
 
+# check for the total number of build in the app engine if it is over a thredshold, for example : 200
+
 for ID in $(gcloud app versions list --sort-by=createTime --format="value(VERSION.ID)")
 do
    ((VERSIONS_COUNT=VERSIONS_COUNT+1))
 done
 
-echo $VERSIONS_COUNT
 
-
+# iterate what service is required  to be built.
+# grap the most out-dated version id of that specific service and delete it
 if [[ $VERSIONS_COUNT -ge $VERSION_MAX ]] 
 then 
 	echo "Deleting the last verion which its count is 200....!!" 
-	gcloud app versions delete $(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)' --service=$SERVICE_NAME) --quiet
+    OUTDATED_VERSION_ID=$(gcloud app versions list --sort-by=createTime --limit 1 --format='value(VERSION.ID)' --service=$SERVICE_NAME)
+	gcloud app versions delete  $OUTDATED_VERSION_ID --quiet
 fi
 
 
